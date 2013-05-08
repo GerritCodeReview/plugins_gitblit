f31d67e3b876160236777be444494f45f58ab817
Luca Milanesio <luca.milanesio@gmail.com>
2013-05-08 16:53:48 +0100

    Support for TopMenu GitBlit item in Gerrit.
    Change-Id: Iec48cacfc897af5fdf9f67aac2d2cfcd62e6905c

---
+++ src/main/java/com/googlesource/gerrit/plugins/gitblit/GitBlitTopMenu.java
@@ -0,0 +1,25 @@
+package com.googlesource.gerrit.plugins.gitblit;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.gerrit.extensions.annotations.Listen;
+import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.extensions.webui.TopMenuExtension;
+import com.google.inject.Inject;
+
+@Listen
+public class GitBlitTopMenu implements TopMenuExtension {
+  private String pluginName;
+
+  @Inject
+  public GitBlitTopMenu(@PluginName String pluginName) {
+    this.pluginName = pluginName;
+  }
+
+  @Override
+  public List<MenuEntry> getEntrys() {
+    return Arrays.asList(new MenuEntry("GitBlit", Arrays.asList(new MenuItem(
+        "Home", "/plugins/" + pluginName))));
+  }
+}
