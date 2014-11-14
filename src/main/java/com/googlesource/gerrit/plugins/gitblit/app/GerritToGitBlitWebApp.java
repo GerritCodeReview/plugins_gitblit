9e865b1896fb0cbc3093d02d108ad696c50170e2
Luca Milanesio <luca.milanesio@gmail.com>
2014-11-14 17:10:55 +0000

    [WIP] Bump to GitBlit 1.7.0-SNAPSHOT
    Change-Id: I11081a077aaad974ca20f29ab0be5d56d69846bb

--- src/main/java/com/googlesource/gerrit/plugins/gitblit/app/GerritToGitBlitWebApp.java
+++ src/main/java/com/googlesource/gerrit/plugins/gitblit/app/GerritToGitBlitWebApp.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2012 The Android Open Source Project
+// Copyright (C) 2014 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,21 +11,52 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 package com.googlesource.gerrit.plugins.gitblit.app;
 
 import org.apache.wicket.protocol.http.WebRequestCycleProcessor;
 import org.apache.wicket.request.IRequestCodingStrategy;
 import org.apache.wicket.request.IRequestCycleProcessor;
 
+import com.gitblit.manager.IAuthenticationManager;
+import com.gitblit.manager.IFederationManager;
+import com.gitblit.manager.IGitblit;
+import com.gitblit.manager.INotificationManager;
+import com.gitblit.manager.IPluginManager;
+import com.gitblit.manager.IProjectManager;
+import com.gitblit.manager.IRepositoryManager;
+import com.gitblit.manager.IRuntimeManager;
+import com.gitblit.manager.IServicesManager;
+import com.gitblit.manager.IUserManager;
+import com.gitblit.tickets.ITicketService;
+import com.gitblit.transport.ssh.IPublicKeyManager;
 import com.gitblit.wicket.GitBlitWebApp;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
 
 public class GerritToGitBlitWebApp extends GitBlitWebApp {
-    @Override
-    protected IRequestCycleProcessor newRequestCycleProcessor() {
-        return new WebRequestCycleProcessor() {
-            protected IRequestCodingStrategy newRequestCodingStrategy() {
-                return new StaticCodingStrategy("summary/", "project/");
-            }
-        };
-    }
+
+  @Inject
+  public GerritToGitBlitWebApp(
+      Provider<IPublicKeyManager> publicKeyManagerProvider,
+      Provider<ITicketService> ticketServiceProvider,
+      IRuntimeManager runtimeManager, IPluginManager pluginManager,
+      INotificationManager notificationManager, IUserManager userManager,
+      IAuthenticationManager authenticationManager,
+      IRepositoryManager repositoryManager, IProjectManager projectManager,
+      IFederationManager federationManager, IGitblit gitblit,
+      IServicesManager services) {
+    super(publicKeyManagerProvider, ticketServiceProvider, runtimeManager,
+        pluginManager, notificationManager, userManager, authenticationManager,
+        repositoryManager, projectManager, federationManager, gitblit, services);
+  }
+
+  @Override
+  protected IRequestCycleProcessor newRequestCycleProcessor() {
+    return new WebRequestCycleProcessor() {
+      protected IRequestCodingStrategy newRequestCodingStrategy() {
+        return new StaticCodingStrategy("summary/", "project/");
+      }
+    };
+  }
 }
