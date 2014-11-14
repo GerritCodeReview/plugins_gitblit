62ee55c8950a6740e139a054169564dd66f47ac0
Luca Milanesio <luca.milanesio@gmail.com>
2014-11-14 17:10:55 +0000

    [WIP] Bump to GitBlit 1.7.0-SNAPSHOT
    Change-Id: I11081a077aaad974ca20f29ab0be5d56d69846bb

--- src/main/java/com/googlesource/gerrit/plugins/gitblit/WrappedSyndicationFilter.java
+++ src/main/java/com/googlesource/gerrit/plugins/gitblit/WrappedSyndicationFilter.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2012 The Android Open Source Project
+// Copyright (C) 2014 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 package com.googlesource.gerrit.plugins.gitblit;
 
 import java.io.IOException;
@@ -20,34 +21,54 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 
-import com.gitblit.SyndicationFilter;
+import com.gitblit.manager.IAuthenticationManager;
+import com.gitblit.manager.IProjectManager;
+import com.gitblit.manager.IRepositoryManager;
+import com.gitblit.manager.IRuntimeManager;
 import com.gitblit.models.UserModel;
+import com.gitblit.servlet.SyndicationFilter;
+import com.google.gerrit.extensions.registration.DynamicItem;
 import com.google.gerrit.httpd.WebSession;
 import com.google.inject.Inject;
-import com.google.inject.Provider;
 import com.google.inject.Singleton;
 import com.googlesource.gerrit.plugins.gitblit.auth.GerritAuthFilter;
 
 @Singleton
 public class WrappedSyndicationFilter extends SyndicationFilter {
-  @Inject
-  private GerritAuthFilter gerritAuthFilter;
+  private final GerritAuthFilter gerritAuthFilter;
+  private final DynamicItem<WebSession> webSession;
+  
+  static class SyndicationHttpServletRequest extends HttpServletRequestWrapper {
+    public SyndicationHttpServletRequest(HttpServletRequest request) {
+      super(request);
+    }
+
+    @Override
+    public String getServletPath() {
+      return super.getServletPath() + "/feed";
+    }
+  }
 
   @Inject
-  private final Provider<WebSession> webSession;
-
-  @Inject
-  public WrappedSyndicationFilter(final Provider<WebSession> webSession) {
-    super();
+  public WrappedSyndicationFilter(DynamicItem<WebSession> webSession,
+      GerritAuthFilter gerritAuthFilter,
+      IRuntimeManager runtimeManager,
+      IAuthenticationManager authenticationManager,
+      IRepositoryManager repositoryManager,
+      IProjectManager projectManager) {
+    super(runtimeManager, authenticationManager, repositoryManager, projectManager);
     this.webSession = webSession;
+    this.gerritAuthFilter = gerritAuthFilter;
   }
 
   @Override
   public void doFilter(ServletRequest request, ServletResponse response,
       FilterChain chain) throws IOException, ServletException {
     if (gerritAuthFilter.doFilter(webSession, request, response, chain)) {
-      super.doFilter(request, response, chain);
+      super.doFilter(new SyndicationHttpServletRequest(
+          (HttpServletRequest) request), response, chain);
     }
   }
 
