b666b8f6693ec57d743dbf0925a4f055ae97a3c2
Luca Milanesio <luca.milanesio@gmail.com>
2016-09-30 14:17:51 +0100

    Fix compilation errors on Gerrit master
    Makes GitBlit plugin compatible with Gerrit master code-base and API.
    
    Change-Id: I369249cb1e0ca34699f6b520a91232d3ee6584af

--- src/main/java/com/googlesource/gerrit/plugins/gitblit/app/GitBlitSettings.java
+++ src/main/java/com/googlesource/gerrit/plugins/gitblit/app/GitBlitSettings.java
@@ -42,22 +42,22 @@ public class GitBlitSettings extends IStoredSettings {
   private static final Logger log = LoggerFactory.getLogger(GitBlitSettings.class);
   private static final String GITBLIT_GERRIT_PROPERTIES = "/gitblit.properties";
 
-  private final LocalDiskRepositoryManager repoManager;
   private final GitBlitUrlsConfig config;
   private final File etcDir;
+  private final File basePath;
 
   private Properties properties;
   private File gitblitPropertiesFile;
 
   @Inject
-  public GitBlitSettings(final LocalDiskRepositoryManager repoManager,
-      final @GerritServerConfig Config config, final SitePaths sitePaths)
+  public GitBlitSettings(@GerritServerConfig Config config,
+      SitePaths sitePaths)
       throws IOException {
     super(GitBlitSettings.class);
     this.properties = new Properties();
-    this.repoManager = repoManager;
     this.config = new GitBlitUrlsConfig(config);
     this.etcDir = sitePaths.etc_dir.toFile();
+    this.basePath = sitePaths.data_dir.toFile();
     load();
   }
 
@@ -112,7 +112,7 @@ public class GitBlitSettings extends IStoredSettings {
   }
 
   public File getBasePath() {
-    return repoManager.getBasePath().toFile();
+    return basePath;
   }
 
   @Override
